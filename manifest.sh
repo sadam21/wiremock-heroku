fd58ba256fa70a89f35f051028224db80a2b2173
Adam Štulpa <adam.stulpa@gooddata.com>
2017-06-05 13:24:54 +0200

    upgrade to wiremock 2.6.0

--- manifest.sh
+++ manifest.sh
@@ -1,2 +1,2 @@
-ARTIFACT_URL=http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/2.5.0/wiremock-standalone-2.5.0.jar
+ARTIFACT_URL=http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/2.6.0/wiremock-standalone-2.6.0.jar
 DOWNLOAD_FROM=public
